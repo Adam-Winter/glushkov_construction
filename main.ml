250e58b4519b1fea23e1b0ee1c6bb37d39ee98b9
adam_nehme <adam@nehme.fr>
2022-11-02 20:49:26 +0100

    Fonction regex_of_string qui transforme une regex en string. Manque beaucoup de cas.

--- main.ml
+++ main.ml
@@ -23,12 +23,21 @@ let rec string_of_regex (exp: regex): string =
 let regex_of_string (s: string): regex =
   let n = String.length s in
   let rec aux (stack: rpn list) (i: int): regex =
-    if List.length stack >= 2 then
-      match stack with
-      | Unary(e1)::Element(e2)::rest -> aux (Element(Star(e2))::rest) ()
-      | Binary(e2)if (s.[i] = '|') || (s.[i] = '@') || (s.[i] = '*') then
-    else 
-      if i = n - 1 then
-        match stack with
-        | Element(e) -> e 
+    match stack with
+    | [] ->
+      if (s.[i] = '∅') then
+        aux (Element(Empty)::[]) (i + 1)
+      else if (s.[i] = 'ε') then
+        aux (Element(Epsilon)::[]) (i + 1)
+      else
+        aux (Element(Letter(s.[i]))::[]) (i +1)
+    | Element(e1)::Element(e2)::rest ->
+      if (s.[i] = '|')  then
+        aux Element(Union(e1, e2)::rest) (i + 1)
+      else if (s.[i] = '@') then 
+        aux Element(Concat(e1, e2)::rest) (i + 1)
+      else if (s.[i] = '*') then
+        aux Element(Star(e1)::Element(e2)::rest) (i + 1)
+      else if s.[i] = '∅' then
+        aux Element()
        
