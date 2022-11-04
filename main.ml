6566902bd767d23b52705d66a03ad6cfc631cc31
adam_nehme <adam@nehme.fr>
2022-11-04 19:10:57 +0100

    Full code

--- main.ml
+++ main.ml
@@ -6,6 +6,13 @@ type regex =
   | Concat of regex * regex
   | Star of regex
 
+type regex2 = 
+  | Letter of int 
+  | Option of regex2
+  | Union of regex2 * regex2
+  | Concat of regex2 * regex2
+  | Star of regex2
+
 type automate = {
   nb_etats: int;
   initiaux: bool array;
@@ -131,6 +138,177 @@ let regex_of_string (s: string): regex =
       else 
         aux (Letter(s.[i])::e1::e2::rest) (i + 1) 
   in aux [] 0
+
+let gestion_point (i: int) (save: (int * char) list)=
+  (* on considère le point comme l'union de toutes les lettres*) 
+  let rec aux (e: regex2) (j: int) (save: (int * char) list)=
+    if j = 128 then
+      e, i + 128, save 
+    else 
+      aux (Union( Letter(i + j), e)) (j+1) (((i+j), (Char.chr j))::save)
+  in 
+  aux (Letter(i)) 1 ((i, (Char.chr 0))::save)
+        
+    
+let regex_of_string_linearized (s: string): regex2 * ((int * char) list) =
+  let len = String.length s in
+  let rec aux (stack: regex2 list) (i: int) (save: (int * char) list) (j: int)  =
+    match stack with 
+    | [] -> 
+        if i = len then
+          failwith "regexp vide"
+        else if s.[i] = '*' || s.[i] = '?' || s.[i] = '|' || s.[i] = '@' then
+          failwith "format regexp invalide "
+        else if s.[i] = '.' then
+          let e, new_j, new_save = gestion_point i save in
+          
+          aux [e] (i+1) new_save new_j
+        else 
+          aux [Letter(i)] (i+1) ((i, s.[i])::save) (j+1)
+    |elt::[] ->
+        if i = len then
+          elt, save
+        else if  s.[i] = '|' || s.[i] = '@' then
+          failwith "format regexp invalide "
+        else if s.[i] = '*' then
+          aux [Star(elt)] (i+1) save j
+        else if s.[i] = '?' then
+          aux [Option(elt)] (i+1) save j
+        else if s.[i] = '.' then
+          let e, new_j, new_save = gestion_point i save in
+          aux [e; elt] (i+1) new_save new_j
+        else 
+          aux [(Letter(j));elt] (i+1) ((j, s.[i])::save) (j+1)
+    |e1::e2::rest -> 
+        if i = len then
+          failwith "format invalide"
+        else if  s.[i] = '|' then
+          aux ((Union(e2,e1))::rest) (i+1) save j
+        else if s.[i] = '@' then
+          aux (Concat(e2, e1)::rest) (i+1) save j
+        else if s.[i] = '*' then
+          aux (Star(e1)::e2::rest) (i+1) save j
+        else if s.[i] = '?' then
+          aux (Option(e1)::e2::rest) (i+1) save j
+        else if s.[i] = '.' then
+          let e, new_j, new_save = gestion_point i save in
+          aux (e::e1::e2::rest) (i+1) new_save new_j
+        else 
+          aux (Letter(j)::e1::e2::rest) (i+1) ((j, s.[i])::save) (j+1)
+  in aux [] 0 [] 0
+    
+let rec get_P (e: regex2) : int list = 
+  match e with
+  | Letter(i) -> [i]
+  | Union(e1, e2) -> (get_P e1)@(get_P e2) (* complexité pas folle *) 
+  | Concat(Star(e1), e2) -> (get_P e1)@(get_P e2)
+  | Star(e1) |Option(e1) |Concat(e1,_)-> get_P e1 
+                                           
+let rec get_S (e: regex2) : int list = 
+  match e with
+  | Letter(i) -> [i]
+  | Union(e1, e2) -> (get_S e1)@(get_S e2) (* complexité pas folle *) 
+  | Concat(e1, Star(e2)) -> (get_S e1)@(get_S e2)
+  | Star(e1) |Option(e1) |Concat(_,e1)-> get_S e1
+                                           
+let produit (x: int) (l: int list): (int * int) list =
+  let rec aux l acc =
+    match l with
+    | [] -> acc
+    | y::ys -> aux ys ((x, y)::acc)
+  in aux l []
+                                           
+let produit_cartesien (l1 : int list) (l2: int list): (int * int) list =
+  let rec aux l1 l2 acc=
+    match l1 with
+    | [] -> acc
+    | x::xs -> aux xs l2 ((produit x l2)@acc) 
+  in aux l1 l2 []
+                                           
+let rec get_F (e: regex2): (int * int) list =
+  match e with
+  | Letter(i) -> []
+  | Union(e1, e2) -> (get_F e1)@(get_F e2)
+  | Concat(e1, e2) -> (get_F e1)@(get_F e2)@(produit_cartesien (get_S e1) (get_P e2))
+  | Star(e1) -> (get_F e1)@(produit_cartesien (get_S e1) (get_P e1))
+  | Option(e1) -> get_F e1
+                    
+let rec decoder (x: int) (codage: (int * char) list): char=
+  match codage with
+  | [] -> failwith "code invalide"
+  | (i, c)::rest ->
+      if i = x then
+        c 
+      else 
+        decoder x rest
+                    
+let rec initialiser_automate_pref (a: automate) (pref: int list) codage =
+  match pref with
+  | [] -> ()
+  | x::xs -> 
+      a.transitions.(a.nb_etats -1) <- ((decoder x codage), x)::a.transitions.(a.nb_etats -1) ;
+      initialiser_automate_pref a xs codage
+        
+let rec initialiser_automate_suff (a: automate) (suff: int list) =
+  match suff with
+  | [] -> ()
+  | x::xs -> 
+      a.terminaux.(x) <- true ;
+      initialiser_automate_suff a xs 
+        
+let rec creation_arcs (a: automate)(fact: (int * int) list) (codage: (int * char) list) =
+  match fact with
+  | [] -> ()
+  | (x1, x2)::xs ->
+      begin
+        a.transitions.(x1) <- ((decoder x2 codage), x2)::a.transitions.(x1);
+        creation_arcs a xs codage
+      end
+                                               
+let glushkov(e: regex2) (codage: (int * char) list): automate =
+  let len = List.length codage +1 in
+  let res = {nb_etats = len; initiaux = Array.make len false; terminaux =  Array.make len false; transitions = Array.make len [] } in
+  let pref = get_P e in
+  let suff = get_S e in
+  let fact = get_F e in
+  begin
+    initialiser_automate_pref res pref codage;
+    initialiser_automate_suff res suff;
+    creation_arcs res fact codage;
+    res.initiaux.(res.nb_etats -1) <- true
+  end;
+  res
+  
+  
+let find_initial (a: automate): int =
+  (* On suppose que a est deterministe donc qu'il n'y a qu'un seul etata inital*)
+  let rec aux i= 
+    if i = a.nb_etats then
+      failwith "pas d'etat inital"
+    else if a.initiaux.(i) then i 
+    else aux (i+1)
+  in aux 0
+        
+      
+let get_next (a: automate) (state: int) (transi: char) =
+  let rec aux (l: (char * int) list) =
+    match l with
+    | [] -> -1
+    |(c, q)::rest -> 
+        if c = transi then q
+        else aux rest 
+  in aux a.transitions.(state)
+  
+let is_recognized (a: automate) (m: string)=
+  let len = String.length m in 
+  let rec aux (curr: int) (i: int) =
+    let next = get_next a curr m.[i] in
+    if next = -1 then
+      false
+    else if i = len-1 then 
+      a.terminaux.(curr)
+    else aux next (i+1)
+  in aux (find_initial a) 0
 
 let recognized_alphabet (a: automate): char list =
   let n: int = a.nb_etats in
